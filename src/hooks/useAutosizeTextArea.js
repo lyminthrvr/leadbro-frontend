4765eef4c611fd958a27afccc0c0f3d79d500ea1
Ni3omi <girasimov1337@gmail.com>
2024-07-17 03:31:35 +0500

    feat: update autosize
    inherit to auto

--- src/hooks/useAutosizeTextArea.js
+++ src/hooks/useAutosizeTextArea.js
@@ -1,20 +1,15 @@
-import {useEffect, useLayoutEffect} from "react";
+import { useEffect, useLayoutEffect } from 'react';
 
-const useAutosizeTextArea = (
-    textAreaRef,
-    value,isRendered,setRendered
-) => {
+const useAutosizeTextArea = (textAreaRef, value, isRendered, setRendered) => {
+  useEffect(() => {
+    if (textAreaRef.current && isRendered) {
+      textAreaRef.current.style.height = 'auto';
+      const scrollHeight = textAreaRef.current.scrollHeight;
 
-
-    useEffect(() => {
-        if (textAreaRef.current && isRendered) {
-            textAreaRef.current.style.height = "inherit";
-            const scrollHeight = textAreaRef.current.scrollHeight;
-
-            textAreaRef.current.style.height = scrollHeight +  "px";
-            setRendered(false)
-        }
-    }, [textAreaRef.current, value,isRendered]);
+      textAreaRef.current.style.height = scrollHeight + 'px';
+      setRendered(false);
+    }
+  }, [textAreaRef.current, value, isRendered]);
 };
 
 export default useAutosizeTextArea;
