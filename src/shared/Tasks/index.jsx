a2ab8358cc4abba076c0b036993936eba80817ec
Ni3omi <girasimov1337@gmail.com>
2024-08-02 19:16:56 +0500

    fix: add touch bakcend on dnd to phone

--- src/shared/Tasks/index.jsx
+++ src/shared/Tasks/index.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
+import { TouchBackend } from 'react-dnd-touch-backend';
 import TaskList from './List';
-
+const isMobile =
+  /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+    navigator.userAgent,
+  );
+const Backend = isMobile ? TouchBackend : HTML5Backend;
 const TasksManager = ({ data, handleChange, counts }) => {
   return (
-    <DndProvider backend={HTML5Backend} options={{ enableMouseEvents: true }}>
+    <DndProvider backend={Backend} options={{ enableMouseEvents: true }}>
       <TaskList data={{ data, counts }} onChange={handleChange} />
     </DndProvider>
   );
