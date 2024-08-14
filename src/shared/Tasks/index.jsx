280780b344f221a432464ca80b4e2afafbb30c9a
Ni3omi <girasimov1337@gmail.com>
2024-08-14 15:59:41 +0500

    wip: trying to add phone scroll on tasks

--- src/shared/Tasks/index.jsx
+++ src/shared/Tasks/index.jsx
@@ -1,19 +1,28 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 import { TouchBackend } from 'react-dnd-touch-backend';
 import TaskList from './List';
+import { useDndScrolling } from 'react-dnd-scrolling';
 const isMobile =
   /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
     navigator.userAgent,
   );
 const Backend = isMobile ? TouchBackend : HTML5Backend;
+
 const TasksManager = ({ data, handleChange, counts }) => {
   return (
-    <DndProvider backend={Backend} options={{ enableMouseEvents: true }}>
+    <DndProvider
+      backend={Backend}
+      options={
+        isMobile
+          ? { enableMouseEvents: true, delayTouchStart: 0, delayMouseStart: 0 }
+          : undefined
+      }
+    >
+      <div>{isMobile ? 'mobile' : 'not mobile'}</div>
       <TaskList data={{ data, counts }} onChange={handleChange} />
     </DndProvider>
   );
 };
-
 export default TasksManager;
