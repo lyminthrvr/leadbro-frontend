91ae5b3431541373d0c9aa4cfc350712cd313bcc
Ni3omi <girasimov1337@gmail.com>
2024-08-15 03:46:12 +0500

    feat: add preview on mobile

--- src/shared/Tasks/Preview/index.jsx
+++ src/shared/Tasks/Preview/index.jsx
@@ -4,25 +4,41 @@ import { useDropTarget } from 'react-dnd/lib/hooks/useDrop/useDropTarget';
 
 const DragPreview = ({ values }) => {
   const { isDragging, item, currentOffset } = useDragLayer((monitor) => ({
-    item: monitor,
+    item: monitor.getItem(),
     itemType: monitor.getItemType(),
     initialOffset: monitor.getInitialSourceClientOffset(),
     currentOffset: monitor.getSourceClientOffset(),
     isDragging: monitor.isDragging(),
   }));
-  return isDragging ? (
-    <div
-      className="preview"
-      style={{
-        position: 'fixed',
-        pointerEvents: 'none',
-        left: 0,
-        top: 0,
-      }}
-    >
-      <Task task={values.find((el) => el.id === item.id)} />
-    </div>
-  ) : null;
+  const draggedTask =
+    item &&
+    values.reduce((acc, column) => {
+      return acc || column.values.find((task) => task.id === item.id);
+    }, null);
+  console.log(
+    item,
+    'item',
+    values,
+    values.find((el) => el.values.find((task) => task.id === item?.id)),
+    draggedTask,
+  );
+
+  return isDragging
+    ? item && (
+        <div
+          className="preview"
+          style={{
+            position: 'fixed',
+            pointerEvents: 'none',
+            left: 0,
+            transform: `translate(${currentOffset?.x}px, ${currentOffset?.y}px)`,
+            top: 0,
+          }}
+        >
+          <Task task={draggedTask} />
+        </div>
+      )
+    : null;
 };
 
 export default DragPreview;
