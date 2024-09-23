fac817efd10a7e4e8a6d17130dd0f0ab4bd041a7
Ni3omi <girasimov1337@gmail.com>
2024-09-23 15:54:16 +0500

    feat: add api to members

--- src/pages/Members/members.api.js
+++ src/pages/Members/members.api.js
@@ -1,35 +1,44 @@
-import {handleHttpError, handleHttpResponse, http, mockHttp} from "../../shared/http";
-import mocks from './members.mocks'
-import useStore from "../../hooks/useStore";
-import {useEffect, useRef} from "react";
+import {
+  handleHttpError,
+  handleHttpResponse,
+  http,
+  mockHttp,
+  resetApiProvider,
+  setMockProvider,
+} from '../../shared/http';
+import mocks from './members.mocks';
+import useStore from '../../hooks/useStore';
+import { useEffect, useRef } from 'react';
+import mapBackendToMock from './members.mapper';
 
-
-mockHttp.onGet('/members').reply(200, mocks.createMembers())
-mockHttp.onPost('/members').reply(200, mocks.createMembers())
-
+mockHttp.onGet('/members').reply(200, mocks.createMembers());
+mockHttp.onPost('/members').reply(200, mocks.createMembers());
 
 const useMembersApi = () => {
+  mockHttp.onGet('/members').reply(200, mocks.createMembers());
+  const { membersStore } = useStore();
+  const getMembers = () => {
+    // setMockProvider();
+    resetApiProvider();
+    return http
+      .get('/api/selector/employees')
+      .then(handleHttpResponse)
+      .then((res) => mapBackendToMock(res.body.data))
+      .then((res) => membersStore.setMembers(res))
+      .then(() => membersStore.getMembers())
+      .catch(handleHttpError);
+  };
 
-    mockHttp.onGet('/members').reply(200, mocks.createMembers())
-    const {membersStore} = useStore()
-    const getMembers = () => {
-        return http.get('/members').then(handleHttpResponse).then((res)=>membersStore.setMembers(res.body)).then(()=>membersStore.getMembers()).catch(handleHttpError)
-    }
+  const setMembers = (body) => {
+    // setMockProvider();
+    return http
+      .post('/members', body)
+      .then(handleHttpResponse)
+      .then((res) => membersStore.setMembers(res.body))
+      .catch(handleHttpError);
+  };
 
-    const setMembers = (body) => {
-        return  http.post('/members',body).then(handleHttpResponse).then((res)=>membersStore.setMembers(res.body)).catch(handleHttpError)
-    }
-    
+  return { setMembers, getMembers };
+};
 
-
-
-    return {setMembers,getMembers}
-}
-
-
-
-
-export default useMembersApi
-
-
-
+export default useMembersApi;
